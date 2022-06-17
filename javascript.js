cb6cbb69d977bb3f0bf14719c9724eeb83d7abca
matelowy <99990518+matelowy@users.noreply.github.com>
2022-06-17 20:56:38 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var przycisk = document.getElementById('button');
+var kontener = document.getElementById("container");
+
+przycisk.addEventListener("click",pierwszaFunkcjaMoja);
+
+function pierwszaFunkcjaMoja(){
+    console.log('działa');
+}
